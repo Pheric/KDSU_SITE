b0ed04468e1d18ba6da0bd7ba2f800e6e6579409
the-rileyj <rileyjohnson99@hotmail.com>
2018-04-21 13:18:48 -0500

    Minor webserver change, styling adjustments

--- server.go
+++ server.go
@@ -10,9 +10,7 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) { http.ServeFile(c.Writer, c.Request, "./index.html") })
-	r.GET("/static/css/:fi", static.Serve("/static/css", static.LocalFile("static/css/", true)))
-	r.GET("/static/img/:fi", static.Serve("/static/img", static.LocalFile("static/img/", true)))
-	r.GET("/static/js/:fi", static.Serve("/static/js", static.LocalFile("static/js/", true)))
-	r.GET("/static/custom/:fi", static.Serve("/static/custom", static.LocalFile("static/custom/", true)))
-	r.Run(":5566")
+	r.GET("/favicon.ico", func(g *gin.Context) { http.ServeFile(g.Writer, g.Request, "/static/img/favicon.ico") })
+	r.Use(static.Serve("/static", static.LocalFile("static/", true)))
+	r.Run(":6969")
 }
