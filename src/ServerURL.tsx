c67102260714eee9b338e87b3659132c794db47a
Riley <rileyjohnson99@hotmail.com>
2019-02-03 22:18:36 -0600

    Minor fix to server URL, preventing CORS issues

--- src/ServerURL.tsx
+++ src/ServerURL.tsx
@@ -1,3 +1,3 @@
-const serverURL = window.location.port === "3000" ? `localhost:8001` : `kdsu.net`
+const serverURL = window.location.port === "3000" ? `localhost:8001` : window.location.host
 
 export default serverURL
