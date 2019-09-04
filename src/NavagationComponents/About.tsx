2387c9fc842244bde92417d98220d76ccb3b2a67
Riley Johnson <rileyjohnson99@hotmail.com>
2019-09-04 10:39:33 -0500

    Updated about page to reflect current meeting status

--- src/NavagationComponents/About.tsx
+++ src/NavagationComponents/About.tsx
@@ -7,10 +7,16 @@ class About extends React.Component {
     public render() {
         return (
             <ContentCard ContentCardBottomLinks={[{ external: false, route: "/contact", text: "Contact" }]} title="About">
-                <div style={{fontSize: "5vh"}}>
-                    KDSU is the <span style={{ fontWeight: "bold" }}> student lead </span> radio organization at Dakota State University.
-
-                    We do live events, on campus radio, as well as online radio.
+                <div style={{ fontSize: "5vh" }}>
+                    <div>
+                        KDSU is the <span style={{ fontWeight: "bold" }}> student led </span> radio organization at Dakota State University.
+                    </div>
+                    <div style={{ marginTop: "1rem" }}>
+                        We primarily apply our music passions, graphic design skills, and software development skills towards providing on campus events such as school dances, promoting events and creating logos, and maintaining internet radio at kdsu.net
+                    </div>
+                    <div style={{ marginTop: "1rem" }}>
+                        KDSU meets <span style={{ fontWeight: "bold" }}>every week on Friday during the DSU school year at the KDSU booth</span> (next to the game area) in the Trojan Center, from <span style={{ fontWeight: "bold" }}>1PM to 2PM.</span>
+                    </div>
                 </div>
             </ContentCard>
         )
